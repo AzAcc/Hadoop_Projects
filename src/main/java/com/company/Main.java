a70ce03981ebadf1e743284ee4945864bb72093b
Alex <zzaaxvat@gmail.com>
2019-12-23 17:34:59 +0300

    Last task

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -16,58 +16,61 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        Configuration conf = new Configuration();
+        Path output = new Path("output\\doc2.txt");
+        FileSystem hdfs = FileSystem.get(conf);
+
+        if (hdfs.exists(output)) {
+            hdfs.delete(output, true);
+        }
+        HashMap<String,String> allUsers = new HashMap<String, String>();
             Job job = Job.getInstance();
             job.setJarByClass(Main.class);
-
-            TextInputFormat.addInputPath(job, new Path("input\\doc1.txt"));
+            TextInputFormat.addInputPath(job, new Path("input\\logs_example.csv"));
             job.setInputFormatClass(TextInputFormat.class);
-            job.setMapperClass(WordCountMapper.class);
-            job.setReducerClass(WordCountReducer.class);
+            job.setMapperClass(LoginMapper.class);
+            job.setReducerClass(LoginReducer.class);
             TextOutputFormat.setOutputPath(job, new Path("output\\doc2.txt"));
             job.setOutputFormatClass(TextOutputFormat.class);
             job.setOutputKeyClass(Text.class);
-            job.setOutputValueClass(IntWritable.class);
+            job.setOutputValueClass(Text.class);
             System.exit(job.waitForCompletion(true)?0:1);
-
 
     }
 
-    public static class WordCountMapper
-            extends Mapper<LongWritable, Text, Text, IntWritable> {
+    public static class LoginMapper
+            extends Mapper<LongWritable, Text, Text, Text> {
         private final static IntWritable one = new IntWritable(1);
         private final Text word = new Text();
         @Override
         protected void map(LongWritable key, Text value, Context context)
                 throws IOException, InterruptedException {
-            StringTokenizer tokenizer = new StringTokenizer(value.toString());
-            while (tokenizer.hasMoreTokens()) {
-                word.set(tokenizer.nextToken());
-                context.write(word, one);
-            }
+            String[] kosmos = value.toString().split(",");
+            //context.write(new Text(kosmos[4]),new Text(kosmos[2]));
+            context.write(new Text(kosmos[2]),new Text(kosmos[4]));
+
         }
     }
-
-    public static class WordCountReducer
-            extends Reducer<Text, IntWritable, Text, IntWritable> {
-
-        public WordCountReducer() {
-        }
-
-        @Override
-        protected void reduce(Text key, Iterable<IntWritable> values,
+    public static class LoginReducer
+            extends Reducer<Text, Text, Text, Text> {
+        protected void reduce(Text key, Iterable<Text> values,
                               Context context)
                 throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable value : values) {
-                sum += value.get();
+            HashSet<String> tempString = new HashSet<>();
+            for(Text value:values){
+                tempString.add(value.toString());
             }
-            context.write(key, new IntWritable(sum));
+            context.write(key,new Text(tempString.toString()));
+
         }
     }
 }
